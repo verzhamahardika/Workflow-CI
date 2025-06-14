dd3bca4e9415bfa840b63987f8721d0cae409ec8
Verzha Mahardika <verzhamahardhika@gmail.com>
2025-06-15 07:59:12 +0800

    Add additional imports for model training and evaluation

--- MLProject/main.py
+++ MLProject/main.py
@@ -1,4 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import mlflow
